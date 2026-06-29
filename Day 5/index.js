98cef67348cd85468d63b3c62961c9cc15ec0961
JayKaushik15 <kaushikjay036@gmail.com>
2026-06-29 10:59:15 +0530

    Day 5 content

---
+++ Day 5/index.js
@@ -0,0 +1,13 @@
+//................Way to create dropdown in js................
+
+// function dataPrint() {
+//     let ele = document.getElementById("color-dropdown")
+//     let res = document.getElementById("result")
+
+//     ele.addEventListener("change",(event)=>{  //data store krega and event handle krega i.e; click.
+//         let selectedValue = event.target.value
+//         res.textContent = `Selected data : ${selectedValue}`        //`` back ticks used to provide and fetch data dynamically.
+//     })                   
+    
+// }
+// dataPrint()
